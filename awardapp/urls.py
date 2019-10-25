63fd3325c3082c6a7fb229b9e75e50646c8b8aed
chastine1 <batamurizashastine@gmail.com>
2019-10-25 19:20:40 +0200

    view

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -15,7 +15,8 @@ urlpatterns=[
     url(r'^search/',  views.search_results,name='search_results'),
     url(r'^projects/(\d+)',views.projects,name='projects'),
     url(r'^ajax/newsletter/$', views.newsletter, name='newsletter'),
-    url(r'^api/merch/$', views.MerchList.as_view())
+    url(r'^api/merch/$', views.MerchList.as_view()),
+    url(r'^api/merch/profile/$', views.MerchListProfile.as_view())
 
 ]
 if settings.DEBUG:
