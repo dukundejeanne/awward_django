131956170004cd9ec930040777d6e5a0da411967
chastine1 <batamurizashastine@gmail.com>
2019-10-25 19:41:48 +0200

    token

--- awardapp/views.py
+++ awardapp/views.py
@@ -10,7 +10,7 @@ from .forms import NewsLetterForm
 from rest_framework.response import Response
 from rest_framework.views import APIView
 # from .models import  MoringaMerch
-from .serializer import MerchSerializer
+from .serializer import MerchSerializer,MerchSerializerProfile
 from rest_framework import status
 
 # display images
