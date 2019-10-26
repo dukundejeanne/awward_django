38b4603d0ae6c4564abe39e209472576ea024300
dukundejeanne <dukundem90@gmail.com>
2019-10-26 19:48:11 +0200

    second

--- award/settings.py
+++ award/settings.py
@@ -59,14 +59,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-Application definition
-
 INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework',
@@ -199,3 +191,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
+
+
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
